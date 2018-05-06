33d5c1709c4747304c6d7f9e34b41eb3b2948023
Tokareva Ekaterina <katetka@bk.ru>
2018-05-06 23:32:59 +0300

    pixel

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task("style", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("c"))
+    .pipe(gulp.dest("source/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"));
